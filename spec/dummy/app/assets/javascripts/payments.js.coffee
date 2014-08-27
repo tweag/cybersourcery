5a58483091a48214fe94867ca61c2dc00aed8491
Michael Toppa <public@toppa.com>
2014-08-27 20:00:03 +0900

    first pass at dummy app setup (not working yet)

---
+++ spec/dummy/app/assets/javascripts/payments.js.coffee
@@ -0,0 +1,21 @@
+$ ->
+  # only show a drop down list of states if the selected country is the US
+  $('#payment_bill_to_address_country').change ->
+    if $(this).val() == 'US'
+      $('#payment_bill_to_address_state').replaceWith($('#payment_bill_to_address_state_select_template').clone().prop({ id: 'payment_bill_to_address_state'}))
+    else
+      $('#payment_bill_to_address_state').replaceWith($('#payment_bill_to_address_state_input_template').clone().prop({ id: 'payment_bill_to_address_state'}))
+
+  # populate the card_expiry_date field from the expiry month and year
+  $('#new_payment').submit ->
+    month = $('#payment_card_expiry_dummy_2i').val()
+    # the transaction will fail without a 2 digit month
+    month = if month.length == 1 then "0#{month}" else month
+    year = $('#payment_card_expiry_dummy_1i').val()
+    $('#payment_card_expiry_date').val("#{month}-#{year}")
+
+    # Cybersource generally ignores fields it's not expecting, but sending arrays it's not expecting
+    # causes a 500 server error, so remove the payment[card_expiry_dummy] fields!
+    $('#payment_card_expiry_dummy_3i').remove()
+    $('#payment_card_expiry_dummy_2i').remove()
+    $('#payment_card_expiry_dummy_1i').remove()
---
+++ spec/dummy/app/controllers/payments_controller.rb
@@ -0,0 +1,62 @@
+class PaymentsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :confirm
+  before_action :normalize_cybersource_params, only: :confirm
+
+  UNSIGNED_FIELD_NAMES = %i[
+    bill_to_email
+    bill_to_forename
+    bill_to_surname
+    bill_to_address_line1
+    bill_to_address_line2
+    bill_to_address_country
+    bill_to_address_state
+    bill_to_address_postal_code
+    bill_to_address_city
+    card_cvn
+    card_expiry_date
+    card_number
+    card_type
+  ]
+
+  def pay
+    setup_payment_form
+  end
+
+  # This receives a POST from the hidden form dynamically rendered in the user's browser by
+  # Cybersource.
+  def confirm
+    profile = Cybersourcery::Profile.new('pwksgem')
+    signature_checker = Cybersourcery::CybersourceSignatureChecker.new({ profile: profile, params: params })
+    signature_checker.run!
+    flash.now[:notice] = Cybersourcery::ReasonCodeChecker::run!(params[:reason_code])
+    serializer = Cybersourcery::MerchantDataSerializer.new
+    @merchant_data = serializer.deserialize(params)
+    #redirect_to profile.success_url # this is optional
+  rescue Cybersourcery::CybersourceryError => e
+    flash.now[:alert] = e.message
+    # if there was an exception in setup_payment_form, it will have already rendered :error
+    render :pay if setup_payment_form
+  end
+
+  private
+
+  def normalize_cybersource_params
+    params.keys.each do |key|
+      params[key[4..-1]] = params[key] if key =~ /^req_/
+    end
+  end
+
+  def setup_payment_form
+    profile = Cybersourcery::Profile.new('pwksgem')
+    signature_checker = Cybersourcery::CartSignatureChecker.new({ profile: profile, params: params, session: session})
+    signature_checker.run!
+    signer = Cybersourcery::CybersourceSigner.new(profile, UNSIGNED_FIELD_NAMES)
+    @payment = Cybersourcery::MyPayment.new(signer, profile, params)
+    true
+  rescue Cybersourcery::CybersourceryError => e
+    # if there was an exception in confirm(), there will already be a flash message
+    flash.now[:alert].present? ? flash.now[:alert] << " #{e.message}" : e.message
+    render :error
+    false
+  end
+end
