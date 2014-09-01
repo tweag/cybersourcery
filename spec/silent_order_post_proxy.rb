1f37cf998d9f862078c7bae89e78b2eb43755297
Michael Toppa <public@toppa.com>
2014-09-01 22:00:04 +0900

    fix the proxy servers setup

--- spec/silent_order_post_proxy.rb
+++ spec/silent_order_post_proxy.rb
@@ -3,12 +3,12 @@ require 'rack/translating_proxy'
 class SilentOrderPostProxy < Rack::TranslatingProxy
 
   def target_host
-    'https://testsecureacceptance.cybersource.com'
+    'http://localhost:5556'
   end
 
   def request_mapping
     {
-      'http://localhost:5556' => target_host,
+      'http://localhost:5555' => target_host,
       'http://cybersourcery.dev/confirm' => 'http://tranquil-ocean-5865.herokuapp.com/confirm',
     }
   end
