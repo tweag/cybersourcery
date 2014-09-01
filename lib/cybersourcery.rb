950cbaec4d19f191045f3e3735376c5719206fd6
Michael Toppa <public@toppa.com>
2014-09-02 08:44:59 +0900

    load certain libs only if we're running in Rails, so we can still load config info from Sinatra

--- lib/cybersourcery.rb
+++ lib/cybersourcery.rb
@@ -1,27 +1,30 @@
 require 'cybersourcery/version'
 require 'cybersourcery/configuration'
 require 'cybersourcery/exceptions'
-require 'cybersourcery/merchant_data_serializer'
-require 'cybersourcery/profile'
-require 'cybersourcery/payment'
-require 'cybersourcery/railtie'
-require 'cybersourcery/cybersource_signer'
-require 'cybersourcery/cart_signer'
-require 'cybersourcery/signature_checker'
-require 'cybersourcery/cart_signature_checker'
-require 'cybersourcery/cybersource_signature_checker'
-require 'cybersourcery/reason_code_checker'
 
-if Rails.env.test?
-  require 'slim-rails'
-  require 'bootstrap-sass'
-  require 'simple_form'
-  require 'sass-rails'
-  require 'coffee-rails'
-  require 'jquery-rails'
-  require 'nokogiri'
-  require 'webmock'
-  require 'vcr'
+if defined? Rails
+  require 'cybersourcery/merchant_data_serializer'
+  require 'cybersourcery/profile'
+  require 'cybersourcery/payment'
+  require 'cybersourcery/railtie'
+  require 'cybersourcery/cybersource_signer'
+  require 'cybersourcery/cart_signer'
+  require 'cybersourcery/signature_checker'
+  require 'cybersourcery/cart_signature_checker'
+  require 'cybersourcery/cybersource_signature_checker'
+  require 'cybersourcery/reason_code_checker'
+
+  if Rails.env.test?
+    require 'slim-rails'
+    require 'bootstrap-sass'
+    require 'simple_form'
+    require 'sass-rails'
+    require 'coffee-rails'
+    require 'jquery-rails'
+    require 'nokogiri'
+    require 'webmock'
+    require 'vcr'
+  end
 end
 
 module Cybersourcery
