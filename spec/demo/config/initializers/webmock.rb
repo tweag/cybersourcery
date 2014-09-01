8a4d98ae2d5e1ad22ac1db1fe22d1e9980411087
Michael Toppa <public@toppa.com>
2014-09-02 08:44:13 +0900

    disable webmock in the Rails app

--- spec/demo/config/initializers/webmock.rb
+++ spec/demo/config/initializers/webmock.rb
@@ -1,4 +1,3 @@
-# WebMock is included for use in the Sinatra app running the proxy server. We don't want to run it
-# here in the main app
-#WebMock.disable!
-WebMock.allow_net_connect!
+# WebMock is included in the gemspec for use in the Sinatra app running the proxy server. We don't
+# want to run it here in the main app.
+WebMock.disable!
