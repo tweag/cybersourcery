f2a5c5e04d03c7ae4a605b2be3afce222184658a
Michael Toppa <public@toppa.com>
2014-09-02 10:11:05 +0900

    check if WebMock is defined before trying to disable it

--- spec/demo/config/initializers/webmock.rb
+++ spec/demo/config/initializers/webmock.rb
@@ -1,3 +1,3 @@
 # WebMock is included in the gemspec for use in the Sinatra app running the proxy server. We don't
 # want to run it here in the main app.
-WebMock.disable!
+WebMock.disable! if defined? WebMock
