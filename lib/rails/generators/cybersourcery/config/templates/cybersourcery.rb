5d294a0c38b4e184ee58b1eaf89771712689f8b9
Michael Toppa <public@toppa.com>
2014-09-02 10:15:55 +0900

    this file is also read by the proxy running on Sinatra, so don't depend on Rails.root

--- lib/rails/generators/cybersourcery/config/templates/cybersourcery.rb
+++ lib/rails/generators/cybersourcery/config/templates/cybersourcery.rb
@@ -1,5 +1,7 @@
 Cybersourcery.configure do |config|
-  config.profiles = "#{Rails.root}/config/cybersourcery_profiles.yml"
+  # this file is also read by the proxy running on Sinatra, so don't depend on Rails.root
+  rails_root = File.expand_path File.dirname(__FILE__) + '/../..'
+  config.profiles = "#{rails_root}/config/cybersourcery_profiles.yml"
   config.sop_live_url = 'https://secureacceptance.cybersource.com'
   config.sop_test_url = 'https://testsecureacceptance.cybersource.com'
   config.sop_proxy_url = 'http://localhost:5555'
