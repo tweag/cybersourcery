564a9eec7e2d451821a54c6af7b1c1a492313035
Michael Toppa <public@toppa.com>
2014-09-02 13:42:06 +0900

    extract reusable parts of target host setup

--- spec/cybersourcery_target_host.rb
+++ spec/cybersourcery_target_host.rb
@@ -1,77 +1,9 @@
-require 'sinatra'
-require 'base64'
-require 'json'
-require 'vcr'
-require 'nokogiri'
-require 'webmock'
-require 'cybersourcery'
-require File.expand_path File.dirname(__FILE__) + '/demo/config/initializers/cybersourcery.rb'
+require 'target_host_setup'
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'spec/cassettes'
-  c.hook_into :webmock
-  c.allow_http_connections_when_no_cassette = true
   c.register_request_matcher :card_number_equality do |request_1, request_2|
     pattern = /\&card_number=(\d+)\&/i
-    body1   = request_1.body
-    body2   = request_2.body
-
-    if body1 =~ pattern && body2 =~ pattern
-      one = pattern.match(body1).captures[0]
-      two = pattern.match(body2).captures[0]
-      one == two
-    else
-      body1 !~ pattern && body2 !~ pattern
-    end
-  end
-end
-
-helpers do
-  def cybersource_proxy_uri
-    "http://#{settings.bind}:#{settings.port}"
-  end
-
-  def hijack_uri(hijacker, victim)
-    hijacker  = URI(hijacker)
-    victim    = URI(victim)
-    generic   = URI::Generic.build(
-      scheme:   hijacker.scheme, userinfo: hijacker.userinfo,
-      host:     hijacker.host,   port:     hijacker.port,
-      path:     victim.path,     query:    victim.query,
-      fragment: victim.fragment
-    )
-    URI(generic.to_s)
-  end
-
-  def hijack_body_uris(body)
-    doc = Nokogiri::HTML(body)
-    %w(src href action).each do |attribute|
-      doc.xpath("//*[@#{attribute}]").each do |node|
-        victim          = node[attribute]
-        hijacker        = hijacker_for_uri(victim)
-        node[attribute] = hijack_uri(hijacker, victim)
-      end
-    end
-    doc.to_html
-  end
-
-  def hijacker_for_uri(victim)
-    if URI(victim).absolute? && victim !~ /cybersource/i
-      request.referrer
-    else
-      cybersource_proxy_uri
-    end
-  end
-
-  def proxy_request
-    cybersource_uri = hijack_uri(Cybersourcery.configuration.sop_test_url, uri)
-    response = yield(cybersource_uri)
-    code     = response.code.to_i
-    type     = response.content_type
-    body     = response.body
-    body     = hijack_body_uris(body) if type =~ /html/
-    response = code, { 'Content-Type' => type }, body
-    response # Grrr. Rubocop.
+    vcr_request_matcher_match?(pattern, request_1.body, request_2.body)
   end
 end
 
