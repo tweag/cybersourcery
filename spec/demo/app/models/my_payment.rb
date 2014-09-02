d051d60dfe216250d3632b379439b965a222fc2b
Michael Toppa <public@toppa.com>
2014-09-02 14:05:29 +0900

    add :card_expiry_dummy attr

--- spec/demo/app/models/my_payment.rb
+++ spec/demo/app/models/my_payment.rb
@@ -1,4 +1,4 @@
 class MyPayment < Cybersourcery::Payment
-  attr_accessor :bill_to_phone, :card_cvn
-  validates_presence_of :card_cvn
+  attr_accessor :bill_to_phone, :card_cvn, :card_expiry_dummy
+  validates_presence_of :card_cvn, :card_expiry_dummy
 end
