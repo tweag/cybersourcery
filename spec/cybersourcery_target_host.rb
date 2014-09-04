330b7b90bd820c4ad407e6a626da014b6aa51abf
Michael Toppa <public@toppa.com>
2014-09-04 15:58:21 +0900

    remove the old Sorcery hijacking code

--- spec/cybersourcery_target_host.rb
+++ spec/cybersourcery_target_host.rb
@@ -16,15 +16,21 @@ get '/*' do |path|
 end
 
 post '/*' do |path|
-  proxy_request do |uri|
-    if Cybersourcery.configuration.use_vcr_in_tests
-      VCR.use_cassette('cybersourcery',
-        record: :new_episodes,
-        match_requests_on: %i(method uri card_number_equality)) do
-        Net::HTTP.post_form(uri, params)
+  uri = URI "#{Cybersourcery.configuration.sop_test_url}/#{path}"
+  response = nil
+
+  if Cybersourcery.configuration.use_vcr_in_tests
+    VCR.use_cassette('cybersourcery',
+      record: :new_episodes,
+      match_requests_on: %i(method uri card_number_equality)) do
+        response = Net::HTTP.post_form(uri, params)
       end
-    else
-      Net::HTTP.post_form(uri, params)
-    end
+  else
+     response = Net::HTTP.post_form(uri, params)
   end
+
+  code     = response.code.to_i
+  type     = response.content_type
+  body     = response.body
+  response = code, { 'Content-Type' => type }, body
 end
