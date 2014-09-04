f19f3493b14d3cbdc055c075845cb503bd1ac44c
Michael Toppa <public@toppa.com>
2014-09-04 20:07:04 +0900

    Get spec working for a failed transaction - see code comments

--- spec/cybersourcery_translating_proxy.rb
+++ spec/cybersourcery_translating_proxy.rb
@@ -11,8 +11,12 @@ class CybersourceryTranslatingProxy < Rack::TranslatingProxy
       # the proxy                what the target host thinks it is
       'http://localhost:5555' => 'https://testsecureacceptance.cybersource.com',
 
+
       # local confirmation page          page where Cybersource redirects
-      'http://localhost:3000/confirm' => 'http://tranquil-ocean-5865.herokuapp.com/confirm'
+      'http://127.0.0.1:3000/confirm' => 'http://tranquil-ocean-5865.herokuapp.com/confirm'
+      # Using the IP address is important here, instead of localhost. The test server runs on
+      # 127.0.0.1, and the app sets a cookie which is checked on /confirm. We need the IP address
+      # to be the same, so the cookie is accessible (fortunately the port number doesn't matter)
     }
   end
 end
