bf7897dd91f9102e2f5da5417bc72a453cbe224a
Michael Toppa <public@toppa.com>
2014-09-10 14:07:34 +0900

    add example custom target_host configuration

---
+++ spec/cybersourcery_target_host.rb
@@ -0,0 +1,9 @@
+require 'sinatra' # weird - why do we have to require sinatra here too?
+require 'cybersourcery_testing/target_host'
+
+VCR.configure do |c|
+  c.register_request_matcher :card_type_equality do |request_1, request_2|
+    pattern = /\&card_type=(\d+)\&/i
+    CybersourceryTesting::Vcr.did_it_change?(pattern, request_1.body, request_2.body)
+  end
+end
