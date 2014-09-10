c3bfff443312ddf0efc330713808f686843ed20f
Michael Toppa <public@toppa.com>
2014-09-10 10:41:57 +0900

    remove use_vcr_in_tests config - that's now a concern of the cybersourcery_testing gem

--- lib/cybersourcery/configuration.rb
+++ lib/cybersourcery/configuration.rb
@@ -1,7 +1,7 @@
 module Cybersourcery
   class Configuration
     attr_reader :profiles
-    attr_accessor :sop_proxy_url, :sop_live_url, :sop_test_url, :use_vcr_in_tests
+    attr_accessor :sop_proxy_url, :sop_live_url, :sop_test_url
 
     def profiles=(profiles_path)
       @profiles = YAML.load_file profiles_path
--- spec/demo/config/initializers/cybersourcery.rb
+++ spec/demo/config/initializers/cybersourcery.rb
@@ -1,9 +1,6 @@
 Cybersourcery.configure do |config|
-  # this file is also read by the proxy running on Sinatra, so don't depend on Rails.root
-  rails_root = File.expand_path File.dirname(__FILE__) + '/../..'
-  config.profiles = "#{rails_root}/config/cybersourcery_profiles.yml"
+  config.profiles = "#{Rails.root}/config/cybersourcery_profiles.yml"
   config.sop_live_url =  ENV['CYBERSOURCERY_SOP_LIVE_URL']
   config.sop_test_url = ENV['CYBERSOURCERY_SOP_TEST_URL']
   config.sop_proxy_url = ENV['CYBERSOURCERY_SOP_PROXY_URL']
-  config.use_vcr_in_tests = ENV['CYBERSOURCERY_USE_VCR_IN_TESTS']
 end
