7c457f3442ee8b3a787cc535c7c8132a8de2f48e
Michael Toppa <public@toppa.com>
2014-09-15 08:56:07 +0900

    update the expected port number for the proxy server

--- spec/features/configuration_spec.rb
+++ spec/features/configuration_spec.rb
@@ -4,7 +4,7 @@ feature 'Cybersourcery configuration' do
   scenario 'configures Cybersourcery with a valid yaml file' do
     expect(Cybersourcery.configuration.profiles['pwksgem']['name']).to eq 'PromptWorks Gem'
     expect(Cybersourcery.configuration.profiles['acptfee']['currency']).to eq 'USD'
-    expect(Cybersourcery.configuration.sop_proxy_url).to eq 'http://localhost:5555'
+    expect(Cybersourcery.configuration.sop_proxy_url).to eq 'http://localhost:5556'
     expect(Cybersourcery.configuration.sop_live_url).to eq 'https://secureacceptance.cybersource.com'
     expect(Cybersourcery.configuration.sop_test_url).to eq 'https://testsecureacceptance.cybersource.com'
   end
