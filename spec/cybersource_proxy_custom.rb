7665490500490c15e53a7631c857eaddb664c4f2
Michael Toppa <public@toppa.com>
2014-09-15 08:56:43 +0900

    update require call for the new name of the proxy config file

--- spec/cybersource_proxy_custom.rb
+++ spec/cybersource_proxy_custom.rb
@@ -1,5 +1,5 @@
 require 'sinatra' # weird - why do we have to require sinatra here too?
-require 'cybersourcery_testing/target_host'
+require 'cybersourcery_testing/cybersource_proxy'
 
 VCR.configure do |c|
   c.register_request_matcher :card_type_equality do |request_1, request_2|
