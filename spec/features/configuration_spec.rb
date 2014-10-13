cada440c6daf2414b022ed1960eb5f8a7486c608
Michael Toppa <public@toppa.com>
2014-10-13 16:32:30 +0900

    make rubucop happy

--- spec/features/configuration_spec.rb
+++ spec/features/configuration_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 feature 'Cybersourcery configuration' do
   scenario 'configures Cybersourcery with a valid yaml file' do
-    expect(Cybersourcery.configuration.profiles['pwksgem']['name']).to eq 'PromptWorks Gem'
-    expect(Cybersourcery.configuration.profiles['pwksgem']['currency']).to eq 'USD'
-    expect(Cybersourcery.configuration.sop_live_url).to eq 'https://secureacceptance.cybersource.com'
-    expect(Cybersourcery.configuration.sop_test_url).to eq 'https://testsecureacceptance.cybersource.com'
+    expect(
+      Cybersourcery.configuration.profiles['pwksgem']['name']
+    ).to eq 'PromptWorks Gem'
+
+    expect(
+      Cybersourcery.configuration.profiles['pwksgem']['currency']
+    ).to eq 'USD'
+
+    expect(
+      Cybersourcery.configuration.sop_live_url
+    ).to eq 'https://secureacceptance.cybersource.com'
+
+    expect(
+      Cybersourcery.configuration.sop_test_url
+    ).to eq 'https://testsecureacceptance.cybersource.com'
   end
 end
-
--- spec/features/payments_spec.rb
+++ spec/features/payments_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 feature 'Payments' do
   context 'Form does not pass client side validation' do
-    scenario 'Fails to submit the payment form when the fields are not filled in', js: true do
+    scenario 'Transaction fails when the fields are not filled in', js: true do
       visit new_cart_path
       click_button 'commit' # submit the cart form
       click_button 'commit' # submit the payment form
 
-      # this is relying on HTML5 validations, which Capybara cannot detect, so check the page header
+      # this is relying on HTML5 validations, which Capybara cannot detect,
+      # so check the page header
       expect(page).to have_content 'New payment'
     end
   end
@@ -29,7 +30,7 @@ feature 'Payments' do
       fill_in 'bill_to_address_postal_code', with: '19083'
     end
 
-    scenario 'Successfully completes a transaction with a valid credit card', js: true do
+    scenario 'Transaction successful with a valid credit card', js: true do
       fill_in 'card_number', with: '4111111111111111'
       select 'Visa', from: 'card_type'
       click_button 'Submit'
@@ -37,15 +38,17 @@ feature 'Payments' do
       expect(page).to have_content 'Successful transaction'
     end
 
-    scenario 'Fails to complete a transaction with an invalid credit card number', js: true do
+    scenario 'Transaction fails with an invalid credit card number', js: true do
       fill_in 'card_number', with: '4111111111111112'
       select 'Visa', from: 'card_type'
       click_button 'Submit'
 
-      expect(page).to have_content 'Declined: One or more fields in the request contains invalid data'
+      expect(page).to have_content(
+        'Declined: One or more fields in the request contains invalid data'
+      )
     end
 
-    scenario 'Fails to complete a transaction with an invalid credit card type', js: true do
+    scenario 'Transaction fails with an invalid credit card type', js: true do
       fill_in 'card_number', with: '4111111111111111'
       select 'American Express', from: 'card_type'
       click_button 'Submit'
